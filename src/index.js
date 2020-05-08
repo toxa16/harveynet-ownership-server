3bd98373612c276573951d580fa6700be076a830
toxa16 <toxa16@gmail.com>
2020-05-08 17:49:57 +0300

    install dotenv

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { Server } = require('http');
 const auth0 = require('auth0');
+require('dotenv').config();   // loading env vars from the ".env" file
 
 const AuthManager = require('./managers/auth-manager');
 const MachineManager = require('./machine-manager');
@@ -7,8 +8,8 @@ const makeApp = require('./make-app');
 
 
 // auth client
-const domain = 'dev-gltebumz.eu.auth0.com';  // env
-const clientId = 'YH7GRHyV7jZQPaXwl9biw50ljZII8a46';  // env
+const domain = process.env.AUTH0_DOMAIN;
+const clientId = process.env.AUTH0_CLIENT_ID;
 const auth0Client = new auth0.AuthenticationClient({ domain, clientId });
 
 const authManager = new AuthManager(auth0Client);
@@ -18,7 +19,7 @@ const machineManager = new MachineManager();
 
 // server
 const server = new Server(makeApp({ authManager, machineManager }));
-const port = process.env.PORT || 3002;
+const port = process.env.PORT;
 server.listen(port, () => {
   console.log('HarveyNet ownership server listening on port '
     + server.address().port + '...');
