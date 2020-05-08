a11d364377ef429ba40770f086dae0353e16c43f
toxa16 <toxa16@gmail.com>
2020-05-08 16:57:59 +0300

    setting unauthorized error name

--- src/errors/unauthorized.js
+++ src/errors/unauthorized.js
@@ -1,3 +1,8 @@
-class Unauthorized extends Error {}
+class Unauthorized extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'Unauthorized';
+  }
+}
 
 module.exports = Unauthorized;
